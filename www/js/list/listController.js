7be8736218d9b22e201fa49355c576a5090e9961
chaowarat <chaowaratt@gmail.com>
2015-09-28 10:41:36 +0700

    generate content text from data

--- www/js/list/listController.js
+++ www/js/list/listController.js
@@ -1,4 +1,4 @@
-﻿define(["app", "js/contactModel","js/list/listView"], function(app, Contact, ListView) {
+﻿define(["app", "js/contactModel", "js/list/listView"], function (app, Contact, ListView) {
 
     var menus = [
         { id: 'menu01', text: 'หน้าแรก', value: '0' },
@@ -10,20 +10,20 @@
         { id: 'menu07', text: 'ออกจากระบบ', value: '6' }
     ];
 
-	var bindings = [{
-	    element: '.list-group li.contact-item',
-		event: 'click',
-		handler: openContact
-	}];
+    var bindings = [{
+        element: '.list-group li.contact-item',
+        event: 'click',
+        handler: openContact
+    }];
 
-	var state = {
-		isFavorite: false
-	};
+    var state = {
+        isFavorite: false
+    };
 
-	function init() {
-	    if (!JSON.parse(localStorage.getItem('templates'))) {
-	        templateInitializeStorage();
-	    }
+    function init() {
+        //if (!JSON.parse(localStorage.getItem('templates'))) {
+        templateInitializeStorage();
+        //}
         var contacts = loadContacts();
         for (var i = 0; i < menus.length; i++) {
             var obj = {
@@ -32,13 +32,13 @@
                 handler: menuClick
             };
             bindings.push(obj);
-        }        
-		ListView.render({
-			bindings: bindings,
-			model: contacts,
-			menu: menus,
-			header: getHeaderName('menu02')
-		});
+        }
+        ListView.render({
+            bindings: bindings,
+            model: contacts,
+            menu: menus,
+            header: getHeaderName('menu02')
+        });
     }
 
     function getHeaderName(id) {
@@ -77,34 +77,34 @@
             i++;
         }
         app.router.load('dailyForm', { id: target.getAttribute('data-id') });
-	}
+    }
 
-	function showAll() {
-		state.isFavorite = false;
-		var contacts = loadContacts();
-		ListView.reRender({ model: contacts, header: "Contacts" });
-	}
+    function showAll() {
+        state.isFavorite = false;
+        var contacts = loadContacts();
+        ListView.reRender({ model: contacts, header: "Contacts" });
+    }
 
-	function showFavorites() {
-		state.isFavorite = true;
-		var contacts = loadContacts({ isFavorite: true });
-		ListView.reRender({ model: contacts, header: "Favorites" });
-	}
+    function showFavorites() {
+        state.isFavorite = true;
+        var contacts = loadContacts({ isFavorite: true });
+        ListView.reRender({ model: contacts, header: "Favorites" });
+    }
 
-	function loadContacts(filter) {
-		var f7Contacts = localStorage.getItem("f7Contacts");
-		var contacts = f7Contacts ? JSON.parse(f7Contacts) : tempInitializeStorage();
-		if (filter) {
-			contacts = _.filter(contacts, filter);
-		}
-		contacts.sort(contactSort);
-		contacts = _.groupBy(contacts, function(contact) { return contact.firstName.charAt(0); });
-		contacts = _.toArray(_.mapValues(contacts, function(value, key) { return { 'letter': key, 'list': value }; }));
-		return contacts;
-	}
+    function loadContacts(filter) {
+        var f7Contacts = localStorage.getItem("f7Contacts");
+        var contacts = f7Contacts ? JSON.parse(f7Contacts) : tempInitializeStorage();
+        if (filter) {
+            contacts = _.filter(contacts, filter);
+        }
+        contacts.sort(contactSort);
+        contacts = _.groupBy(contacts, function (contact) { return contact.firstName.charAt(0); });
+        contacts = _.toArray(_.mapValues(contacts, function (value, key) { return { 'letter': key, 'list': value }; }));
+        return contacts;
+    }
 
-	function tempInitializeStorage() {
-		var contacts = [
+    function tempInitializeStorage() {
+        var contacts = [
 			new Contact({ "firstName": "Alex", "lastName": "Black", "company": "Global Think", "phone": "+380631234561", "email": "ainene@umail.com", "city": "London", isFavorite: true }),
 			new Contact({ "firstName": "Kate", "lastName": "Shy", "company": "Big Marketing", "phone": "+380631234562", "email": "mimimi@umail.com", "city": "Moscow" }),
 			new Contact({ "firstName": "Michael", "lastName": "Fold", "company": "1+1", "email": "slevoc@umail.com", "city": "Kiev", isFavorite: true }),
@@ -119,15 +119,15 @@
 			new Contact({ "firstName": "Alex", "lastName": "Proti", "company": "Samsung", "phone": "+380631234568", "email": "kate@umail.com", "city": "Kiev" }),
 			new Contact({ "firstName": "Oleg", "lastName": "Ryzhkov", "company": "Unilever", "phone": "+380631234568", "email": "uni@umail.com", "city": "Praha", isFavorite: true }),
 			new Contact({ "firstName": "Daniel", "lastName": "Ricci", "company": "Finni", "phone": "+380631234570", "email": "agent@umail.com", "city": "Milan" })
-		];
-		localStorage.setItem("f7Contacts", JSON.stringify(contacts));
-		return JSON.parse(localStorage.getItem("f7Contacts"));
-	}
+        ];
+        localStorage.setItem("f7Contacts", JSON.stringify(contacts));
+        return JSON.parse(localStorage.getItem("f7Contacts"));
+    }
 
-	function templateInitializeStorage() {
-	    var templates = [
+    function templateInitializeStorage() {
+        var templates = [
         {
-            id: '001', name: 'แบบฟอร์มที่ 1', content: generateContent('001'),
+            id: '001', name: 'แบบฟอร์มที่ 1', content: '',
             data: [
                 {
                     sectionId: 1, sectionName: 'ด้านที่ 1',
@@ -165,27 +165,42 @@
                 }
             ], selected: true
         },
-        { id: '002', name: 'แบบฟอร์มที่ 2', content: generateContent('002'), data: [] },
-        { id: '003', name: 'แบบฟอร์มที่ 3', content: generateContent('003'), data: [] }
-	    ];
-	    localStorage.setItem("templates", JSON.stringify(templates));
-	}
+        { id: '002', name: 'แบบฟอร์มที่ 2', content: '', data: [] },
+        { id: '003', name: 'แบบฟอร์มที่ 3', content: '', data: [] }
+        ];
+        for (var i = 0; i < templates.length; i++) {            
+            templates[i].content = generateContent(templates[i].data);
+        }
+        localStorage.setItem("templates", JSON.stringify(templates));
+    }
 
-	function generateContent(id) {
-	    var text = 'The content of ' + id;
-	    text += '<br /> Card with header and footer. <br />Card header is used to display card title and footer for some additional information or for custom actions.';
-	    return text;
-	}
+    function generateContent(data) {
+        var text = '';
+        for (var i = 0; i < data.length; i++) {
+            text += data[i].sectionName + ' ';
+            
+            for (var j = 0; j < data[i].data.length && j < 3; j++) {
+                text += data[i].data[j].qText + '...';
+            }
+            if (i < data.length - 1) {
+                text += '<br>';
+            }
+        }
+        if (text.length == 0) {
+            text = '...ไม่พบข้อมูลแบบฟอร์ม...';
+        }
+        return text;
+    }
 
-	function contactSort(a, b) {
-		if (a.firstName > b.firstName) {
-			return 1;
-		}
-		if (a.firstName === b.firstName && a.lastName >= b.lastName) {
-			return 1;
-		}
-		return -1;
-	}
+    function contactSort(a, b) {
+        if (a.firstName > b.firstName) {
+            return 1;
+        }
+        if (a.firstName === b.firstName && a.lastName >= b.lastName) {
+            return 1;
+        }
+        return -1;
+    }
 
     return {
         init: init
