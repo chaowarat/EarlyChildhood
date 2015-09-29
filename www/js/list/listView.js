bc4597c1e1261a6a833348de22063c73350c4c82
chaowarat <chaowaratt@gmail.com>
2015-09-29 14:52:32 +0700

    fix unbind click evt after menu changed

--- www/js/list/listView.js
+++ www/js/list/listView.js
@@ -9,10 +9,22 @@ define(['hbs!js/list/contact-list-item', 'hbs!js/list/contact-list-menu'], funct
         bindEvents(params.bindings);
     }
 
-	function reRender(params) {
+    function reRender(params) {
+        removeEvents(params.bindings);
 		$('.contacts-list ul').html(template(params.model));
 		setHeaderText(params.header);
-        $('.searchbar-cancel').click();
+		$('.searchbar-cancel').click();		
+		bindEvents(params.bindings);
+	}
+
+	function removeEvents(bindings) {
+	    for (var i in bindings) {
+	        $(bindings[i].element).on(bindings[i].event, bindings[i].handler);
+	        if (bindings[i].element.removeEventListener)
+	            bindings[i].element.removeEventListener(bindings[i].event, bindings[i].handler, false);
+	        if (bindings[i].element.detachEvent)
+	            bindings[i].element.detachEvent('on' + bindings[i].event, bindings[i].handler);
+	    }
 	}
 
 	function bindEvents(bindings) {
