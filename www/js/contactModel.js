dfa369dcb44b7760aea839aaab760b73c778b585
chaowarat <chaowaratt@gmail.com>
2015-10-20 11:15:33 +0700

    fix navBar and search by class-room

--- www/js/contactModel.js
+++ www/js/contactModel.js
@@ -1,11 +1,12 @@
-define(['app'],function(app) {
+﻿define(['app'],function(app) {
 
     function Contact(values) {
 		values = values || {};
 		this.id = values['id'] || app.utils.generateGUID();
 		this.picId = values['picId'] || app.utils.getRandomInt(1,10);
 		this.createdOn = values['createdOn'] || new Date();
-
+		this.class = values['class'] || 'อนุบาล';
+		this.room = values['room'] || 1;
 		this.firstName = values['firstName'] || '';
 		this.lastName = values['lastName'] || '';
 		this.company = values['company'] || '';
